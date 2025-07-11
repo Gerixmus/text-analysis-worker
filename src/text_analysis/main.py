d5543fbfe6e5cd7e9a4f38d31093cf80dae98738
Gerixmus <gerixmuss@gmail.com>
2025-07-12 01:37:14 +0200

    chore: add bucket retrieval and upload process

--- src/text_analysis/main.py
+++ src/text_analysis/main.py
@@ -4,31 +4,27 @@ import argparse
 from google.cloud import storage
 
 def main():
-    storage_client = storage.Client(project="text-analysis-465423")
-    buckets = storage_client.list_buckets()
-
-    print("Buckets:")
-    for bucket in buckets:
-        print(bucket.name)
+    client  = storage.Client(project="text-analysis-465423")
+    bucket = client.bucket("text-analysis-input")
 
     summarizer = Summarizer("facebook/bart-large-cnn")
 
     parser = argparse.ArgumentParser()
-    parser.add_argument("--in", dest="input_path", required=True, help="Path to input text file")
-    parser.add_argument("--out", dest="output_path", required=True, help="Path to output text file")
+    parser.add_argument("--in", dest="input_blob_name", required=True, help="Blob name")
+    parser.add_argument("--out", dest="output_blob_name", required=True, help="Blob name")
     args = parser.parse_args()
 
-    input_path = Path(args.input_path)
-    if not input_path.exists():
-        raise FileNotFoundError(f"Input file does not exist: {input_path}")
-    
-    with open(input_path, "r", encoding="utf-8") as f:
-        text = f.read()
+    input_blob_name = args.input_blob_name
+    input_blob = bucket.blob(input_blob_name)
+
+    with input_blob.open("r", encoding="utf-8") as f:
+        text = str(f.read())
         output = summarizer.summarize(text)
 
-    output_path = Path(args.output_path)
-    with open(output_path, "w") as f:
-        f.write(output)
+    output_blob_name = args.output_blob_name
+    output_blob = bucket.blob(output_blob_name)
+
+    output_blob.upload_from_string(output)
 
 if __name__ == "__main__":
     main()
